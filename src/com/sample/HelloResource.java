d52f5a4f9bb610d040d84b485e7255a3a84d47fa
1noseA <1noseA2L@gmail.com>
2021-11-06 15:31:13 +0900

    POSTパラメータ

--- src/com/sample/HelloResource.java
+++ src/com/sample/HelloResource.java
@@ -2,10 +2,11 @@ package com.sample;
 
 import java.io.InputStream;
 
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -23,10 +24,31 @@ public class HelloResource {
 //		return new Employee(9999, "sample", "sales", new Date(), 500, 10);
 //	}
 
-	@GET
+//	@GET
+//	@Produces("application/json")
+//	// 引数にアノテーション受け取った値をそこに入れる
+//	public Employee sample(@QueryParam("empno") int empno) {
+//
+//		try {
+//			String resource = "config.xml";
+//			InputStream inputStream = Resources.getResourceAsStream(resource);
+//			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+//
+//			SqlSession session = sqlSessionFactory.openSession();
+//			EmpDao dao = session.getMapper(EmpDao.class);
+//
+//			return dao.findByPrimarykey(empno);
+//
+//		} catch (Exception e) {
+//			throw new RuntimeException(e);
+//		}
+
+//	}
+
+	@POST
 	@Produces("application/json")
 	// 引数にアノテーション受け取った値をそこに入れる
-	public Employee sample(@QueryParam("empno") int empno) {
+	public Employee sample(@FormParam("empno") int empno) {
 
 		try {
 			String resource = "config.xml";
@@ -41,7 +63,6 @@ public class HelloResource {
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
-
 	}
 
 	// /hello/testでアクセス
